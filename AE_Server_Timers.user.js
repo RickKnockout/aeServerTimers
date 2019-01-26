0a75cd2afa0ef27a23e211118b3773c2145de8ce
cruentus <rickkayo@gmail.com>
2019-01-26 15:23:49 -0800

    Update AE_Server_Timers.user.js

--- AE_Server_Timers.user.js
+++ AE_Server_Timers.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                AE Server Timers
 // @namespace           http://www.tampermonkey.net
-// @author              Rick / Aetheric
+// @author              Rick 
 // @description         Replaces local with server times
 // @require http://code.jquery.com/jquery-1.7.1.min.js
 // @include             http://*.astroempires.com/*
