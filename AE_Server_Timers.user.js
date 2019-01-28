b9ba93662415d0ba87f3724fdb5ab9359582fbe1
cruentus <rickkayo@gmail.com>
2019-01-28 14:42:14 -0800

    Update AE_Server_Timers.user.js

--- AE_Server_Timers.user.js
+++ AE_Server_Timers.user.js
@@ -1,9 +1,16 @@
 // ==UserScript==
-// @name                AE Server Timers Beta
+// @name                AE Server Timers BETA
 // @namespace           http://www.tampermonkey.net
-// @author              Rick 
+// @author              Rick
 // @description         Replaces local with server times
 // @require http://code.jquery.com/jquery-1.7.1.min.js
+// @grant GM_setValue
+// @grant GM_getValue
+// @grant GM_setClipboard
+// @grant unsafeWindow
+// @grant window.close
+// @grant window.focus
+// @grant GM_addStyle
 // @include             http://*.astroempires.com/*
 // @exclude             http://*.astroempires.com/
 // @exclude             http://*.astroempires.com/home.aspx*
@@ -32,3 +39,9 @@ window.addEventListener('beforescriptexecute',
     }
 	if(changed == 0) window.removeEventListener(event.type, arguments.callee, true); // If it's done changing shit, stop!
 }, true);
+
+if (document.location.href.indexOf('bases_production') == -1) {
+	window.t = new_t;
+	window.timers = new_timers;
+	window.timersDisplay_convert_timer = function (){};
+};
